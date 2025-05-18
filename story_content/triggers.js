ef8dc46f61314046ce320623c31b44ccd5116658
farischoirudin <149812392+mobilelearn@users.noreply.github.com>
2025-05-18 10:53:42 +0700

    BaruValid

---
+++ story_content/triggers.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6SJg07M9yQn":
+        Script1();
+        break;
+      case "6IgS4Icu83a":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
